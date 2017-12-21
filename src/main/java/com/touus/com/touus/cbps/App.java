88a4f16c568be30808fd36f5d084d717a6abd702
wangfeng <wonge@5s-online.com>
2017-12-21 16:39:53 +0800

    创建一个cbps项目-测试Git

---
+++ src/main/java/com/touus/com/touus/cbps/App.java
@@ -0,0 +1,13 @@
+package com.touus.com.touus.cbps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
